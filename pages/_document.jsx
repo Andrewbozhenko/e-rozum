8a0096844321f422396ffdde8eff37ce0631e2e6
Oleksandr Moskalets <116104431+MoskaletsOleksandr@users.noreply.github.com>
2024-04-04 16:30:41 +0300

    gtm-added

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,9 +1,23 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 const Document = () => {
+  const GTM_ID = "GTM-XXXXXXX";
+
   return (
     <Html lang="uk">
       <Head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+        </Script>
+        {/* End Google Tag Manager */}
         <link
           rel="preload"
           href="/fonts/e-UkraineHead-Bold.woff2"
@@ -60,6 +74,11 @@ const Document = () => {
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
